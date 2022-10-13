97fb98c6b7ef86d4838d1f91eecd57917e9e6d99
manonaidinian <manonaidinian@gmail.com>
2022-10-13 17:09:37 +0200

    created charity model

---
+++ app/models/charity.rb
@@ -0,0 +1,8 @@
+class Charity < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :location, presence: { message: "we need to know where you are located please" }
+  validates :description, allow_nil: true
+
+end
